b87f0fdd11c160aa1a1a3a56bdb1a4d0f03a8882
m.prashanth <prashanthmangena@gmail.com>
2023-05-07 04:07:53 +0530

    classroom.py

--- classroom.py
+++ classroom.py
@@ -2,18 +2,22 @@
 #Start writing your code here
 
 class Classroom:
-    classroom_list=[]
+    classroom_list=['G015', 'G066', 'L123', 'L135', 'L143', 'L13']
     
     @staticmethod
     def search_classroom(class_room):
+        class_room=str(class_room).upper()
         for room in Classroom.classroom_list:
-            if str(class_room).upper()==room.upper():
-                return "Found"
+            if (class_room==room and class_room.startswith("L")):
+                Value="Found"
+                break
             else:
-                return -1
+                Value=-1
             
-Classroom.classroom_list.extend(['G015', 'G066', 'L123', 'L135', 'L143', 'L13'])
+        return Value
+    
+#Classroom.classroom_list.extend(['G015', 'G066', 'L123', 'L135', 'L143', 'L13'])
 
 #print(Classroom.classroom_list)
-print(Classroom.search_classroom(13))
+print(Classroom.search_classroom("l135"))
 #print(Classroom.search_classroom("Phy"))
